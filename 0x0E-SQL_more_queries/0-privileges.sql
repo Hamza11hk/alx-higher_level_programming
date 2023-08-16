ee8598dccbdd3bb8aa68a76a0547df1314c9f367
Hamza11hk <Hamzahk110077@gmail.com>
2023-08-16 16:04:27 -0700

    sql queries tasks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- My privileges
--- show privs for user1
-SHOW GRANTS FOR user_0d_1@localhost;
--- show privs for user2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- script that lists all privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
